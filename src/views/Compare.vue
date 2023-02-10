bb0c437b0f10228ba8080b4636cc45bb46d66c1f
abbcecek <cecilia.ek@abbgymnasiet.se>
2023-02-10 09:04:05 +0100

    Justerat listan, nu ser den bra ut

--- src/views/Compare.vue
+++ src/views/Compare.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen flex items-center flex flex-col">
-    <h1 class="my-4 text-2xl">Välj städer</h1>
+    <h1 class="my-4 text-2xl">Välj länder</h1>
     <span class="absolute inset-y-0 left-0 flex items-center pl-2">
       <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg>
     </span>
@@ -13,7 +13,7 @@
 
     <div
       class="
-        w-11/12
+        w-full
         md:w-4/5
         h-2/3
         grid grid-cols-3
@@ -27,27 +27,57 @@
         rounded-lg
       "
     >
-      <button
+      <label
         v-for="city in filteredList()"
         :key="city"
         class="
           text-white
-          sm:text-lg
-          md:text-2xl
           flex
           items-center
           justify-center
           bg-themeblue-100
           rounded-2xl
-          h-20
-          focus:ring-4
-          hover:bg-themeblue-200
+          h-28
+          grid grid-cols-3
         "
       >
-        {{ city }}
-      </button>
+        <input
+          type="checkbox"
+          class="
+            form-tick
+            h-4
+            w-4
+            md:h-6 md:w-6
+            mx-2
+            md:mx-4
+            border border-gray-300
+            rounded-md
+            checked:bg-blue-600 checked:border-transparent
+            focus:outline-none
+          "
+        />
+        <p class="sm:text-md md:text-2xl">{{ city }}</p>
+      </label>
       <div class="item error" v-if="input && !filteredList().length"></div>
     </div>
+    <button
+      class="
+        text-white text-md
+        md:text-xl
+        flex
+        items-center
+        justify-center
+        my-4
+        bg-themeblue-100
+        active:bg-themeblue-200
+        focus:ring-4
+        rounded-2xl
+        h-20
+        w-1/3
+      "
+    >
+      Bekräfta
+    </button>
   </div>
 </template>
 
@@ -55,7 +85,7 @@
 import { ref } from "vue"
 let input = ref("")
 const cities = [
-  'Västerås', 'Stockholm', 'Eskilstuna', 'Örebro', 'Göteborg', 'Lund', 'Jönköping', 'Uppsala', 'Malmö', 'Köping', 'Norrköping', 'Borås', 'Hjo', 'Nyköping', 'Falköping', 'Söderköping', 'Gävle', 'Växsjö', 'Kalmar'
+  'Sverige', 'Norge', 'Finland', 'Danmark', 'Tyskland', 'Frankrike', 'Nederländerna', 'Island', 'Belgien', 'Österrike', 'Luxemburg', 'Spanien', 'Portugal', 'Polen', 'Albanien', 'Serbien', 'Bosnien', 'Grekland', 'Kroatien', 'Tjeckien', 'Estland', 'Lettland', 'Litauen', 'Ukraina', 'Ungern', 'Bulgarien', 'Rumänien', 'Montenegro', 'Moldavien', 'Monaco', 'Vatikanstaten', 'Makedonien'
 ]
 
 function filteredList () {
