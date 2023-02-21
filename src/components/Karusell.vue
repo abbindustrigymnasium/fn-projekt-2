766b55ea90ae09dfcfe9366904d97ef8b9792278
abbcecek <cecilia.ek@abbgymnasiet.se>
2023-02-21 15:32:01 +0100

    Fixade bildknapparna längst ner
    Jag försökte göra sidan reaktiv men det gick inte så bra för vi har inte så mycket tid

--- src/components/Karusell.vue
+++ src/components/Karusell.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
-    <Carousel
-      :wrap-around="true"
-      :breakpoints="breakpoints"
-      :autoplay="10000"
-    >
+    <Carousel :wrap-around="true" :breakpoints="breakpoints" :autoplay="10000">
       <Slide
         v-for="slide in slides"
         :key="slide.id"
         class="pb-4 2xl:h-SHeight3 md:h-SHeight2 h-SHeight1 z-10"
       >
-        <div class="p-4 w-full h-full relative flex justify-center content-center">
-          <img :src="`${slide.src}`" class="w-full h-full object-cover filter brightness-50" />
-          <p class="absolute text-4xl text-white px-16 self-center">
-            {{ slide.text }}
-          </p>
+        <div
+          class="p-4 w-full h-full relative flex justify-center content-center"
+        >
+          <img
+            :src="`${slide.src}`"
+            class="w-full h-full object-cover filter brightness-50"
+          />
+          <div
+            class="
+              absolute
+              text-4xl text-white
+              px-1
+              md:px-4
+              self-center
+              w-3/4
+              md:w-2/3
+            "
+          >
+            <p class="text-2xl md:text-4xl font-medium">
+              {{ slide.title }}
+            </p>
+            <p class="text-sm md:text-xl mt-2">{{ slide.text }}</p>
+          </div>
         </div>
       </Slide>
       <template #addons>
-        <Navigation />
+        <Navigation class="text-white hover:text-white" />
         <Pagination />
       </template>
     </Carousel>
@@ -43,13 +57,12 @@ export default {
   data () {
     return {
       slides: [
-        { id: 1, title: 'Myrkotte', text: 'Folk tror att jag är en kotte', src: 'https://i.natgeofe.com/k/880b23a6-d3a5-46e1-a60f-2dfbf9208377/pangolin_main_16x9.jpg' },
-        { id: 2, title: 'Röd panda', text: 'Jag råkade ramla ned i falurödfärg', src: 'https://myanimals.com/sv/wp-content/uploads/2020/12/rod-panda.jpg' },
-        { id: 3, title: 'Valross', text: 'Jag är en blandning av elefant, val och säl', src: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Noaa-walrus22.jpg' },
-        { id: 4, title: 'Isbjörn', text: 'Isbjörn gillar att laga mat', src: 'https://tidningensyre.se/wp-content/uploads/2018/05/18cb7db26d38eeec35243c3eced685d1.jpg' },
-        { id: 5, title: 'Esel', text: 'Är jag en åsna?', src: 'https://images.rsa-sachsen.de/files/2021-08/GettyImages-585282190.jpg?rect=445%2C0%2C3559%2C2670&fit=crop&crop=faces&w=500&h=375&dpr=3&fm=webp' },
-        { id: 6, title: 'Stad', text: 'Stad Stad Stad Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu non diam phasellus vestibulum.', src: 'https://usercontent.one/wp/www.arkitekturupproret.se/wp-content/uploads/2017/02/HUVUDBILD-650x325.jpg' },
-        { id: 7, title: 'Stad', text: '', src:"https://media.tenor.com/lASY8qJLPaAAAAAC/angry-black-cat-meme.gif" },
+        { id: 1, title: 'Lorem', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec justo in leo tempus efficitur sed sed felis. Aenean consequat lacus nec tincidunt iaculis. Sed nulla nisl, mollis at mi fermentum, sodales iaculis risus. Integer lectus leo, molestie at tincidunt a, dignissim quis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.', src: 'https://images.unsplash.com/photo-1610028290816-5d937a395a49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80' },
+        { id: 2, title: 'Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut orci mi. Ut congue risus nec facilisis dictum. Suspendisse potenti. Suspendisse tincidunt leo nulla, varius iaculis lorem vehicula pellentesque. Nulla facilisi. Aenean nec faucibus nulla. Suspendisse erat mi, egestas quis tortor laoreet, consequat posuere tellus. Aenean finibus viverra arcu, in.', src: 'https://images.unsplash.com/photo-1531558506007-fe311a2f4729?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80' },
+        { id: 3, title: 'Lipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pretium, metus in vestibulum sollicitudin, sem est cursus augue, in semper justo orci in massa. Mauris velit lorem, tempor in nisl vel, venenatis placerat lectus. Sed commodo diam a lacus hendrerit accumsan. Vivamus tellus ipsum, varius.', src: 'https://images.unsplash.com/photo-1558597879-04d6dfc5216d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' },
+        { id: 4, title: 'Shmipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas egestas lectus ullamcorper dictum lobortis. Sed nec dui rhoncus, imperdiet ante vel, sagittis risus. Curabitur et porta felis. Nunc sit amet felis ut eros rutrum lobortis. Ut ultricies rhoncus neque, vel malesuada elit porta sit amet. Praesent eget egestas lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras ac ante interdum, gravida tellus eget.', src: 'https://images.unsplash.com/photo-1566543042578-5db95b961375?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' },
+        { id: 5, title: 'Klipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat felis sed sollicitudin egestas. Duis a tincidunt eros. Integer lacinia aliquam purus. Duis at odio libero. Proin diam nisi, commodo non velit at, accumsan commodo dui. Aliquam vulputate facilisis pharetra. Suspendisse lobortis.', src: 'https://images.unsplash.com/photo-1604420929906-190b6ed3cd1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80' },
+        { id: 6, title: 'Blablipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ante metus, vehicula vel lectus et, consectetur mollis dui. Ut nec tincidunt nisl. Nullam condimentum eleifend consectetur. Curabitur blandit risus turpis, vitae elementum ex viverra eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce.', src: 'https://images.unsplash.com/photo-1627332314956-f5f597d72d11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' },
       ]
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,59 +3,22 @@
     <div class="mb-16">
       <Karusell />
     </div>
-    <div class="flex row mt-28">
-      <div class="h-96 mr-10 ml-10">
-        <h1 class="text-xl mb-5">Rubrik</h1>
-        <div class="flex row jutify-evenly">
-        <div class="flex justify-between row text-left mb-20">
-          <div class="w-1/2 flex flex-col">
-            <p class=" mx-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. A diam maecenas sed enim ut sem viverra aliquet. Semper eget duis at tellus. Fames ac turpis egestas sed tempus
-            urna et pharetra. Vel turpis nunc eget lorem dolor sed viverra. Eu ultrices vitae auctor eu augue ut lectus arcu
-            bibendum. Ac auctor augue mauris augue neque gravida in. Consequat id porta nibh venenatis cras sed felis. Sagittis orci
-            a scelerisque purus semper eget duis. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. In massa tempor
-            nec feugiat nisl pretium fusce id velit.</p>
-            <p class="mx-3 mt-2">
-            Vulputate ut pharetra sit amet aliquam id. Eget magna fermentum iaculis eu non diam. Dictum varius duis at consectetur
-            lorem donec massa sapien faucibus. Ultrices mi tempus imperdiet nulla. Sem et tortor consequat id.
-            </p>
-          </div>
-      <div class="w-1/2 flex flex-col place-items-center">
-        <p class=" mx-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-          et dolore magna
-          aliqua. A diam maecenas sed enim ut sem viverra aliquet. Semper eget duis at tellus. Fames ac turpis egestas sed
-          tempus
-          urna et pharetra. Vel turpis nunc eget lorem dolor sed viverra. Eu ultrices vitae auctor eu augue ut lectus arcu
-          bibendum. Ac auctor augue mauris augue neque gravida in. Consequat id porta nibh venenatis cras sed felis. Sagittis
-          orci
-          a scelerisque purus semper eget duis.</p>
-        <p class="mx-2 mt-2">
-          Vulputate ut pharetra sit amet aliquam id. Eget magna fermentum iaculis eu non diam. Dictum varius duis at
-          consectetur
-          lorem donec massa sapien faucibus. Ultrices mi tempus imperdiet nulla. Sem et tortor consequat id. 
-        </p>
-        <div class="flex row w-full justify-center space-x-10">
-        <router-link to="/compare" class="h-8 w-1/4 bg-themeblue-200 p-1 text-white rounded-full transform hover:scale-105 transition duration-500 ease-in-out hover:bg-themeblue-100 flex place-content-center mt-6">Jämför</router-link>
-        <router-link to="/top10"
-          class="h-8 w-1/4 bg-themeblue-200 text-white p-1 rounded-full transform hover:scale-105 transition duration-500 ease-in-out hover:bg-themeblue-100 flex place-content-center mt-6">Top 10</router-link>
-      </div>
-      </div>
-      </div>
-      </div>
-        
-      
-      </div>
-    </div>
+    <HomeText />
+    <CompT10 />
   </div>
 </template>
 
 <script>
 import Karusell from '@/components/Karusell.vue'
+import HomeText from '@/components/HomeText.vue'
+import CompT10 from '@/components/CompareOrTop10.vue'
+
 export default {
   name: 'Home',
   components: {
-
-    Karusell
+    Karusell,
+    HomeText,
+    CompT10
   }
 }
 </script>
